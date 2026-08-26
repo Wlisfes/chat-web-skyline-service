7d76dd91ce50fe0b929cc7bbf73690b721a8e119
妖雨纯 <876451336@qq.com>
2026-08-26 22:29:27 +0800

    ✨ feat(App): 优化根节点布局

--- web/components/layout/App.vue
+++ web/components/layout/App.vue
@@ -1,50 +1,34 @@
 <script lang="tsx">
-import { setup as setupCssRender } from '@css-render/vue3-ssr'
-import { defineComponent, h, type App, type DefineComponent, type PropType } from 'vue'
+import { defineComponent, App, PropType, Fragment, h } from 'vue'
+import { Omix } from '@wlisfes/chat-web-base-schema'
+import { setup } from '@css-render/vue3-ssr'
 import { RouterView } from 'vue-router'
-
-const AppRouterView = RouterView as unknown as DefineComponent<{ asyncData: { value: unknown } }>
 
 export default defineComponent({
     name: 'SkylineApp',
+    components: { AppRouterView: RouterView },
     props: {
-        ssrApp: {
-            type: Object as PropType<App>,
-            required: true
-        },
-        asyncData: {
-            type: Object as PropType<{ value: unknown }>,
-            required: true
-        },
-        fetchData: {
-            type: null as unknown as PropType<unknown>,
-            default: undefined
-        },
-        reactiveFetchData: {
-            type: null as unknown as PropType<unknown>,
-            default: undefined
-        },
-        ctx: {
-            type: null as unknown as PropType<unknown>,
-            default: undefined
-        },
-        config: {
-            type: null as unknown as PropType<unknown>,
-            default: undefined
-        }
+        /** Vue SSR 应用实例，用于收集并注入 Naive UI 服务端渲染样式 */
+        ssrApp: { type: Object as PropType<App>, required: true },
+        /** 页面与布局合并后的异步数据响应式容器 */
+        asyncData: { type: Object as PropType<Omix>, required: true },
+        /** 当前路由预取数据的非响应式兼容字段 */
+        fetchData: { type: Object as PropType<Omix> },
+        /** 路由切换后实时更新的页面与布局预取数据容器 */
+        reactiveFetchData: { type: Object as PropType<Omix> },
+        /** 服务端渲染期间的 HTTP 请求上下文 */
+        ctx: { type: Object as PropType<Omix> },
+        /** SSR 框架传入的当前运行时配置 */
+        config: { type: Object as PropType<Omix> }
     },
     setup(props) {
-        const { collect } = setupCssRender(props.ssrApp)
-        const CssRenderCollector = defineComponent({
-            name: 'CssRenderCollector',
-            setup: () => () => h('css-render-style', { innerHTML: collect() })
-        })
+        const { collect } = setup(props.ssrApp)
 
         return () => (
-            <>
-                <AppRouterView asyncData={props.asyncData} />
-                {!__isBrowser__ && <CssRenderCollector />}
-            </>
+            <Fragment>
+                <app-router-view asyncData={props.asyncData} />
+                {!__isBrowser__ && h('css-render-style', { innerHTML: collect() })}
+            </Fragment>
         )
     }
 })
