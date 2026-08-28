b5f9ce456b4b74e7928609f22b925a3cb0a19ab2
妖雨纯 <876451336@qq.com>
2026-08-28 09:35:02 +0800

    refactor: rely on Nacos runtime defaults

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,16 +13,6 @@ import { AppService } from './app.service'
                 registerPort: 4020,
                 NACOS_SERVER: process.env.NACOS_SERVER,
                 NACOS_NAMESPACE: process.env.NACOS_NAMESPACE,
-                NACOS_USERNAME: process.env.NACOS_USERNAME,
-                NACOS_PASSWORD: process.env.NACOS_PASSWORD,
-                NACOS_REQUEST_TIMEOUT: process.env.NACOS_REQUEST_TIMEOUT,
-                NACOS_CONFIG_DATA_ID: process.env.NACOS_CONFIG_DATA_ID,
-                NACOS_CONFIG_GROUP: process.env.NACOS_CONFIG_GROUP,
-                NACOS_REGISTER_ENABLED: process.env.NACOS_REGISTER_ENABLED,
-                NACOS_REGISTER_REQUIRED: process.env.NACOS_REGISTER_REQUIRED,
-                NACOS_GROUP: process.env.NACOS_GROUP,
-                NACOS_REGISTER_IP: process.env.NACOS_REGISTER_IP,
-                NACOS_REGISTER_PORT: process.env.NACOS_REGISTER_PORT,
                 PORT: process.env.PORT
             })
         )
