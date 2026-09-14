18751e4b4d8ba3761aac8c4df7e17b421083af0a
妖雨纯 <876451336@qq.com>
2026-09-14 08:07:30 +0800

    fix: 修复 Skyline 流水线格式校验

--- src/modules/chunk/dto/chunk.dto.ts
+++ src/modules/chunk/dto/chunk.dto.ts
@@ -5,7 +5,10 @@ import { PageDto } from '@wlisfes/chat-web-base-schema/utils'
 import { PageResponseDataDto } from '@wlisfes/chat-web-base-schema/decorator'
 
 /** 枚举字典分页查询参数。 */
-export class ListChunkDto extends IntersectionType(PageDto, PartialType(PickType(TbSkylineChunkDto, ['module', 'type', 'pid', 'status'] as const))) {}
+export class ListChunkDto extends IntersectionType(
+    PageDto,
+    PartialType(PickType(TbSkylineChunkDto, ['module', 'type', 'pid', 'status'] as const))
+) {}
 
 /** 枚举字典主键参数。 */
 export class ChunkKeyDto {
